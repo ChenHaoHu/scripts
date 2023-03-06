362edeb7e20e5f3dac65f48b59c434e5cc9d51da
huchenhao <chenhaohu1998@sina.com>
2023-03-06 07:53:19 +0000

    add github_no_changelog/remove.js

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Record some interesting scripts
 
 1、[VPN agent proxy](vpn_agent_proxy/proxy.go)
 > share your VPN with your friends, and you may need to bypass your VPN agent's check logic
+
+2、[github no changelog](github_no_changelog/remove.js)
+> delete useless changelog display on github homepage
---
+++ github_no_changelog/remove.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         github no changelog
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  remove useless github changelog
+// @author       You
+// @match        https://github.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var targetNode = document.getElementsByClassName("team-left-column")[1];
+
+    // 创建一个 MutationObserver 实例
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            console.log(mutation)
+            var changlogDom = document.getElementsByClassName("dashboard-changelog")[0];
+            if (changlogDom != undefined){
+                changlogDom.remove()
+            }
+        });
+    });
+
+    // Configure MutationObserver Listener
+    var config = { childList: true };
+
+    // Start listening for changes in DOM elements
+    observer.observe(targetNode, config);
+})();
